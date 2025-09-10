26ce0f4d3ac186242be0c464c3fac4fdb70c0a0c
Víctor Alfredo García Calvillo <vgarciac2100@alumno.ipn.mx>
2025-09-10 10:49:15 -0600

    :card_file_box: Conectar a base de datos

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+dotenv.config({ path: process.cwd() + '/.env' });
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URL || '')],
   controllers: [AppController],
   providers: [AppService],
 })
