3013b6666cf6f62b9c53fbf4789c0b79809873b5
Víctor Alfredo García Calvillo <vgarciac2100@alumno.ipn.mx>
2025-09-12 12:03:34 -0600

    :art: Enable CORS por fin

--- apps/client/pages/api/axios.ts
+++ apps/client/pages/api/axios.ts
@@ -4,7 +4,9 @@ const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'Content-Type': 'application/json',
+        
     },
+    //withCredentials: true,
 })
 
 export default instance
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
   app.enableCors({
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  });
+  })
+  await app.listen(4000);
 }
 bootstrap();
