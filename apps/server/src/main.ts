0b3736f177157a5e4ad900b6f71e743d3d1a4fb2
Víctor Alfredo García Calvillo <vgarciac2100@alumno.ipn.mx>
2025-09-12 10:59:13 -0600

    :art: Enable CORS 2

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
+    //credentials: true,
   });
 }
 bootstrap();
